71047cd4f73295124962ba2903131ca0a9c1993d
tosinori-m <your.email@example.com>
2022-07-19 16:08:12 +0900

    Add threemonths_goal_page

--- app/controllers/threemonths_goals_controller.rb
+++ app/controllers/threemonths_goals_controller.rb
@@ -1,18 +1,21 @@
 class ThreemonthsGoalsController < ApplicationController
   before_action :authenticate_user!
 
+  def new
+    form = Form::ThreemonthsGoalCollection.new
+  end
+
   def create
-    threemonths_goals_params = ThreemonthsGoal.new(threemonths_goals_params)
-    threemonths_goals = threemonths_goals_params
-    threemonths_goals.user = current_user
-    threemonths_goals.aim = params[:aim]
-    if threemonths_goals.save
+    quarter_0 = ThreemonthsGoal.new(aim:params[:form_threemonths_goal_collection][:aim_1], quarter: 0,user_id: current_user.id)
+    quarter_1 = ThreemonthsGoal.new(aim:params[:form_threemonths_goal_collection][:aim_2], quarter: 1,user_id: current_user.id)
+    quarter_2 = ThreemonthsGoal.new(aim:params[:form_threemonths_goal_collection][:aim_3], quarter: 2,user_id: current_user.id)
+    quarter_3 = ThreemonthsGoal.new(aim:params[:form_threemonths_goal_collection][:aim_4], quarter: 3,user_id: current_user.id)
+    if quarter_0.save && quarter_1.save && quarter_2.save && quarter_3.save
       render json: { id: threemonths_goals.id, message: '成功しました' }, status: :ok
     else
       render json: { message: '保存出来ませんでした', errors: threemonths_goals.errors.messages }, status: :bad_request
     end
   end
-
 
   def index
     threemonths_goals = ThreemonthsGoal.all
@@ -30,8 +33,10 @@ class ThreemonthsGoalsController < ApplicationController
     render json: threemonths_goals_array, status: 200
   end
 
-  private
-  def threemonths_goals_params
-    params.require(:threemonths_goal).permit(:aim)
+private
+  def threemonths_goal_collection_params
+    params
+      .require(:form_threemonths_goal_collection)
+      .permit(:aim_1, :aim_2, :aim_3, :aim_4)
   end
 end
--- app/models/threemonths_goal.rb
+++ app/models/threemonths_goal.rb
@@ -2,4 +2,5 @@ class ThreemonthsGoal < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :aim, presence: true, length: { maximum: 140 }
+  attr_accessor :collection
 end
