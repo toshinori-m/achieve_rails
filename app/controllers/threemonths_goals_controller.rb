80f75f82d6c0b2335697f47ecd34623bc77dd217
tosinori-m <your.email@example.com>
2022-07-22 14:10:17 +0900

    Add threemonths_goal_page

--- app/controllers/threemonths_goals_controller.rb
+++ app/controllers/threemonths_goals_controller.rb
@@ -1,33 +1,17 @@
 class ThreemonthsGoalsController < ApplicationController
   before_action :authenticate_user!
 
-  # def new
-  #   form = Form::ThreemonthsGoalCollection.new
-  # end
-
   def create
-    # threemonths_goals = ThreemonthsGoal.new()
-    # threemonths_goals.user = current_user
-    # threemonths_goals.aim = params[:aim_1]
-    # threemonths_goals.quarter = 1
-    # quarter = threemonths_goals
-
-    # quarter = ThreemonthsGoal.new(aim:params[:aim_1], quarter: 1, user_id: current_user.id)
-
-    # quarter_1 = ThreemonthsGoal.new(aim:params[:form_threemonths_goal_collection][:aim_2], quarter: 1,user_id: current_user.id)
-    # quarter_2 = ThreemonthsGoal.new(aim:params[:form_threemonths_goal_collection][:aim_3], quarter: 2,user_id: current_user.id)
-    # quarter_3 = ThreemonthsGoal.new(aim:params[:form_threemonths_goal_collection][:aim_4], quarter: 3,user_id: current_user.id)
-
     aims = [params[:aim_1], params[:aim_2], params[:aim_3], params[:aim_4]]
-      for i in 1 .. 4 do threemonths_goals_params = ThreemonthsGoal.new(aims, quarter: i, user_id: current_user.id)
-        quarter = threemonths_goals_params
-
+    for i in 0..3 do
+    threemonths_goals_params = ThreemonthsGoal.new(aim: aims[i], quarter: i, user_id: current_user.id)
+    quarter = threemonths_goals_params
       if quarter.save
-        render json: { id: threemonths_goals.id, message: '成功しました' }, status: :ok
       else
         render json: { message: '保存出来ませんでした', errors: threemonths_goals.errors.messages }, status: :bad_request
       end
     end
+    render json: { id: threemonths_goals_params.id, message: '成功しました' }, status: :ok
   end
 
   # def index
